3a0bf6e7c03d2250f46cc455c92de4015f9bdfd4
speedi33 <hohm.lukas@gmail.com>
2022-12-14 17:13:44 +0100

    First try to generate mermaid string

--- dist/index.js
+++ dist/index.js
@@ -9696,15 +9696,30 @@ const core = __nccwpck_require__(9935);
 const github = __nccwpck_require__(2835);
 const bash = __nccwpck_require__(2081);
 
+const splitAtFirstOccurrence = (string, delimiter) => {
+    return string.substring(string.indexOf(delimiter) + 1);
+}
+
+const generateMermaidGitGraphString = (gitLogString) => {
+    const gitLogLines = gitLogString.split('\n');
+    let mermaidGitGraphString = 'gitGraph\n';
+    for (const gitLogLine in gitLogLines) {
+        const commitId = splitAtFirstOccurrence(gitLogLine, ' ');
+        mermaidGitGraphString += `commit id: "${commitId}"\n`;
+    }
+    return mermaidGitGraphString;
+}
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   const gitLogFile = 'git.log';
   console.log(`Hello ${nameToGreet}!`);
-  bash.execSync(`git log --pretty=oneline --all --reflog > ${gitLogFile}`);
+  bash.execSync(`git log --pretty=oneline --all --reflog --decorate --reverse --pretty=format:"%h %s @%d"> ${gitLogFile}`);
   const gitLog = bash.execSync(`cat ${gitLogFile}`).toString().trim();
   bash.execSync(`rm ${gitLogFile}`);
   console.log(`Your Git Log:\n${gitLog}`);
+  console.log(`Your Mermaid string:\n${generateMermaidGitGraphString(gitLog)}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const bash = require('child_process');
 
+const splitAtFirstOccurrence = (string, delimiter) => {
+    return string.substring(string.indexOf(delimiter) + 1);
+}
+
+const generateMermaidGitGraphString = (gitLogString) => {
+    const gitLogLines = gitLogString.split('\n');
+    let mermaidGitGraphString = 'gitGraph\n';
+    for (const gitLogLine in gitLogLines) {
+        const commitId = splitAtFirstOccurrence(gitLogLine, ' ');
+        mermaidGitGraphString += `commit id: "${commitId}"\n`;
+    }
+    return mermaidGitGraphString;
+}
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   const gitLogFile = 'git.log';
   console.log(`Hello ${nameToGreet}!`);
-  bash.execSync(`git log --pretty=oneline --all --reflog > ${gitLogFile}`);
+  bash.execSync(`git log --pretty=oneline --all --reflog --decorate --reverse --pretty=format:"%h %s @%d"> ${gitLogFile}`);
   const gitLog = bash.execSync(`cat ${gitLogFile}`).toString().trim();
   bash.execSync(`rm ${gitLogFile}`);
   console.log(`Your Git Log:\n${gitLog}`);
+  console.log(`Your Mermaid string:\n${generateMermaidGitGraphString(gitLog)}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
