e3d1a6bcb4a36bfe5c32997287c51369f48e7c65
speedi33 <hohm.lukas@gmail.com>
2022-12-14 20:44:14 +0100

    different approach

--- index.js
+++ index.js
@@ -5,16 +5,59 @@ const fs = require('fs');
 
 let featureBranchCounter = 0;
 
+const isSpecialLine = (gitLogLine, indicator) => {
+    if (gitLogLine.startsWith(indicator)) {
+        return true;
+    } else if (gitLogLine.startsWith('| ')) {
+        return isCommitLine(gitLogLine.substring(2));
+    } else {
+        return false;
+    }
+}
+
+const isCommitLine = (gitLogLine) => {
+    return isSpecialLine(gitLogLine, '*');
+}
+
+const isBranchLine = (gitLogLine) => {
+    return isSpecialLine(gitLogLine, '|/');
+}
+
+const isMergeLine = (gitLogLine) => {
+    return isSpecialLine(gitLogLine, '|\\');
+}
+
 const generateMermaidGitGraphString = (gitLogString) => {
-    const gitLogLines = gitLogString.split('\n');
+    const gitLogLines = gitLogString.split('\n').slice().reverse();
     let mermaidGitGraphString = 'gitGraph\n';
-    for (const gitLogLine of gitLogLines) {
+    gitLogLines.forEach((gitLogLine, index) => {
+        console.log(`>>>${gitLogLine}<<<`);
+        if (isCommitLine(gitLogLine)) {
+            const gitLogLineWithoutAsterisk = gitLogLine.split('*')[1];
+            const commitDetails = gitLogLineWithoutAsterisk.split('-');
+            const commitId = commitDetails[0].trim();
+            mermaidGitGraphString += `  commit id: "${commitId}"\n`;
+        } else if (isBranchLine(gitLogLine)) {
+            //const branchName = tryToGetBranchNameFromNextLine(gitLogLines[index-1]);
+            console.log(`nextLine = <${gitLogLines[index-1]}>`);
+            console.log(`currentLine = <${gitLogLine}>`);
+            console.log(`nextLine = <${gitLogLines[index+1]}>`);
+            mermaidGitGraphString += `  branch xxx\n  checkout xxx\n`;
+        }
+    });
+    for (const [index, gitLogLine] of gitLogLines.entries()) {
+        
+
+
+
+
+/*
         const commitDetails = gitLogLine.split('||');
         const commitId = commitDetails[0];
         const commitParentIds = commitDetails[1];
         if (commitParentIds && commitParentIds.includes(' ')) {
-            const featureBranchName = `feature_branch_${featureBranchCounter}`;
             // merge commit!
+            const featureBranchName = `feature_branch_${featureBranchCounter}`;
             const firstParentCommitId = commitParentIds.split(' ')[0];
             if (mermaidGitGraphString.includes(`id: "${firstParentCommitId}"\n`)) {
                 mermaidGitGraphString = mermaidGitGraphString.replace(
@@ -26,6 +69,8 @@ const generateMermaidGitGraphString = (gitLogString) => {
         } else {
             mermaidGitGraphString += `  commit id: "${commitId}"\n`;
         }
+        previousCommitId = commitId;
+        */
     }
     return mermaidGitGraphString;
 }
@@ -56,7 +101,7 @@ try {
   const nameToGreet = core.getInput('who-to-greet');
   const gitLogFile = 'git.log';
   console.log(`Hello ${nameToGreet}!`);
-  bash.execSync(`git log --pretty=oneline --all --reflog --decorate --reverse --pretty=format:"%h||%p||%s||%d" > ${gitLogFile}`);
+  bash.execSync(`git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all > ${gitLogFile}`);
   const gitLog = bash.execSync(`cat ${gitLogFile}`).toString().trim();
   bash.execSync(`rm ${gitLogFile}`);
   console.log(`Your Git Log:\n${gitLog}`);
